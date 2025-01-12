04233e83647a0e46f5da591b53868c2b68c0a3ae
Ankit Pathak <pathakankit688@gmail.com>
2025-01-12 09:26:51 +0530

    company update

--- Backend/routes/company.route.js
+++ Backend/routes/company.route.js
@@ -7,12 +7,13 @@ import {
   registerCompany,
   updateCompany,
 } from "../controllers/company.controller.js";
+import { singleUpload } from "../middleware/multer.js";
 
 const router = express.Router();
 
 router.route("/register").post(authenticateToken, registerCompany);
 router.route("/get").get(authenticateToken, getAllCompanies);
 router.route("/get/:id").get(authenticateToken, getCompanyById);
-router.route("/update/:id").put(authenticateToken, updateCompany);
+router.route("/update/:id").put(authenticateToken, singleUpload, updateCompany);
 
 export default router;
