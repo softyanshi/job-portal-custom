68489356a6593cff1ed4a21db2905e150bf9b9fc
Ankit Pathak <pathakankit688@gmail.com>
2025-01-23 19:43:39 +0530

    com

--- Frontend/src/components/components_lite/Browse.jsx
+++ Frontend/src/components/components_lite/Browse.jsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./Navbar";
 import Job1 from "./Job1";
-
-const randomJobs = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+import { useDispatch, useSelector } from "react-redux";
+import { setSearchedQuery } from "@/redux/jobSlice";
+import useGetAllJobs from "@/hooks/useGetAllJobs";
 
 const Browse = () => {
+  useGetAllJobs();
+  const { allJobs } = useSelector((store) => store.job);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    return () => {
+      dispatch(setSearchedQuery(""));
+    };
+  }, []);
   return (
     <div>
       <Navbar />
       <div className="max-w-7xl mx-auto my-10">
-        <h1 className="font-bold text-xl my-10 ">Search Results {randomJobs.length}</h1>
+        <h1 className="font-bold text-xl my-10 ">
+          Search Results {allJobs.length}
+        </h1>
         <div className="grid grid-cols-3 gap-4  ">
-          {randomJobs.map((item, index) => {
-            return <Job1 />;
+          {allJobs.map((job) => {
+            return <Job1 key={job._id} job={job} />;
           })}
         </div>
       </div>
--- Frontend/src/hooks/useGetAllJobs.jsx
+++ Frontend/src/hooks/useGetAllJobs.jsx
@@ -1,25 +1,29 @@
- 
 import { setAllJobs } from "@/redux/jobSlice";
 import { JOB_API_ENDPOINT } from "@/utils/data";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const useGetAllJobs = () => {
   const dispatch = useDispatch();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const { searchedQuery } = useSelector((store) => store.job);
 
   useEffect(() => {
     const fetchAllJobs = async () => {
       setLoading(true);
       setError(null);
       try {
-        const res = await axios.get(`${JOB_API_ENDPOINT}/get`, {
-          withCredentials: true,
-        });
+        const res = await axios.get(
+          `${JOB_API_ENDPOINT}/get?keyword=${searchedQuery}`,
+          {
+            withCredentials: true,
+          }
+        );
         console.log("API Response:", res.data);
-        if (res.data.status) { // Updated success check
+        if (res.data.status) {
+          // Updated success check
           dispatch(setAllJobs(res.data.jobs));
         } else {
           setError("Failed to fetch jobs.");
