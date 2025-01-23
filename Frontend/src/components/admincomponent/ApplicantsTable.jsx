853bcf6badcd50e6c2ed2f5229e57ed1a2ec7cc9
Ankit Pathak <pathakankit688@gmail.com>
2025-01-23 18:58:38 +0530

    accept

--- Frontend/src/components/admincomponent/ApplicantsTable.jsx
+++ Frontend/src/components/admincomponent/ApplicantsTable.jsx
@@ -73,7 +73,7 @@ const ApplicantsTable = () => {
                     <span>NA</span>
                   )}
                 </TableCell>
-                <TableCell>{item?.applicant.createdAt.split("T")[0]}</TableCell>
+                <TableCell>{item?.applicant?.createdAt.split("T")[0]}</TableCell>
                 <TableCell className="float-right cursor-pointer">
                   <Popover>
                     <PopoverTrigger>
--- Frontend/src/components/components_lite/AppliedJob.jsx
+++ Frontend/src/components/components_lite/AppliedJob.jsx
@@ -26,17 +26,31 @@ const AppliedJob = () => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {allAppliedJobs.length <= 0 ? <span>You have not applied any job yet. </span> :
+          {allAppliedJobs.length <= 0 ? (
+            <span>You have not applied any job yet. </span>
+          ) : (
             allAppliedJobs.map((appliedJob) => (
               <TableRow key={appliedJob._id}>
                 <TableCell>{appliedJob?.createdAt.split("T")[0]}</TableCell>
                 <TableCell>{appliedJob.job?.title}</TableCell>
                 <TableCell>{appliedJob.job?.company.name}</TableCell>
                 <TableCell className="text-right">
-                  <Badge> {appliedJob?.status}</Badge>{" "}
+                  <Badge
+                    className={`${
+                      appliedJob?.status === "rejected"
+                        ? "bg-red-500"
+                        : appliedJob?.status === "accepted"
+                        ? "bg-green-600"
+                        : "bg-gray-500"
+                    }`}
+                  >
+                    {" "}
+                    {appliedJob?.status}
+                  </Badge>{" "}
                 </TableCell>
               </TableRow>
-            ))}
+            ))
+          )}
         </TableBody>
       </Table>
     </div>
