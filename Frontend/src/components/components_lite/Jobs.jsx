ac3a1db21d72f50b8d3562c2415b29de3aabf270
Ankit Pathak <pathakankit688@gmail.com>
2025-01-23 21:06:37 +0530

    update

--- Frontend/src/components/components_lite/Jobs.jsx
+++ Frontend/src/components/components_lite/Jobs.jsx
@@ -3,6 +3,7 @@ import Navbar from "./Navbar";
 import FilterCard from "./Filtercard";
 import Job1 from "./Job1";
 import { useSelector } from "react-redux";
+import { motion } from "framer-motion";
 
 const Jobs = () => {
   const { allJobs, searchedQuery } = useSelector((store) => store.job);
@@ -10,7 +11,7 @@ const Jobs = () => {
 
   useEffect(() => {
     // If no search query is provided, reset to all jobs
-    //     if (searchedQuery) 
+    //     if (searchedQuery)
     if (!searchedQuery || searchedQuery.trim() === "") {
       setFilterJobs(allJobs);
       return;
@@ -46,9 +47,15 @@ const Jobs = () => {
             <div className="flex-1 h-[88vh] overflow-y-auto pb-5">
               <div className="grid grid-cols-3 gap-4">
                 {filterJobs.map((job) => (
-                  <div key={job.id}>
+                  <motion.div
+                    initial={{ opacity: 0, x: 100 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    exit={{ opacity: 0, x: -100 }}
+                    transition={{ duration: 0.4 }}
+                    key={job.id}
+                  >
                     <Job1 job={job} />
-                  </div>
+                  </motion.div>
                 ))}
               </div>
             </div>
