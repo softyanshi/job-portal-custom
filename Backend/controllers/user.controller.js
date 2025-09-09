313e936af09c7ac37d0d2afc6e4a47aeff179a59
softyanshi <104715288+softyanshi@users.noreply.github.com>
2025-09-09 22:56:33 +0530

    Update user.controller.js
    change in sameSite for allowing different domain

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -137,7 +137,8 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "Strict",
+        sameSite: "None",
+        secure: true,  
       })
       .json({
         message: `Welcome back ${user.fullname}`,
